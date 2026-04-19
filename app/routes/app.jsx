58f17a58f4c7b0696b3e42862b1128b7fc1bfdcd
rockhoundstudio <you@example.com>
2026-04-19 09:55:48 -0700

    add polaris css import

--- app/routes/app.jsx
+++ app/routes/app.jsx
@@ -2,6 +2,7 @@ import { Outlet, useLoaderData, useRouteError } from "react-router";
 import { boundary } from "@shopify/shopify-app-react-router/server";
 import { AppProvider } from "@shopify/shopify-app-react-router/react";
 import { authenticate } from "../shopify.server";
+import "@shopify/polaris/build/esm/styles.css";
 
 export const loader = async ({ request }) => {
   await authenticate.admin(request);
