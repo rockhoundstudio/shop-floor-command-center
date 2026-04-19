28c20aadf2ee51e745d28a7132af7b74385b0df7
rockhoundstudio <you@example.com>
2026-04-19 10:08:07 -0700

    fix polaris SSR add noExternal to vite config

--- vite.config.js
+++ vite.config.js
@@ -2,10 +2,6 @@ import { reactRouter } from "@react-router/dev/vite";
 import { defineConfig } from "vite";
 import tsconfigPaths from "vite-tsconfig-paths";
 
-// Related: https://github.com/remix-run/remix/issues/2835#issuecomment-1144102176
-// Replace the HOST env var with SHOPIFY_APP_URL so that it doesn't break the Vite server.
-// The CLI will eventually stop passing in HOST,
-// so we can remove this workaround after the next major release.
 if (
   process.env.HOST &&
   (!process.env.SHOPIFY_APP_URL ||
@@ -44,13 +40,15 @@ export default defineConfig({
     port: Number(process.env.PORT || 3000),
     hmr: hmrConfig,
     fs: {
-      // See https://vitejs.dev/config/server-options.html#server-fs-allow for more information
       allow: ["app", "node_modules"],
     },
   },
   plugins: [reactRouter(), tsconfigPaths()],
   build: {
     assetsInlineLimit: 0,
+  },
+  ssr: {
+    noExternal: ["@shopify/polaris"],
   },
   optimizeDeps: {
     include: ["@shopify/app-bridge-react"],
